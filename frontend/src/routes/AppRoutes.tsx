ec17dec365528811f42c606926583408b1caed04
V1ctorGbr <vg1771049@gmail.com>
2026-09-03 15:56:48 -0300

    feat: register new routes in AppRoutes

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -5,6 +5,10 @@ import Monitoramento from '../pages/Monitoramento'
 import MainLayout from '../layouts/MainLayout'
 import Alertas from '../pages/Alertas'
 import Eventos from '../pages/Eventos'
+import Usinas from '../pages/Usinas'
+import Subestacoes from '../pages/Subestacoes'
+import Transformadores from '../pages/Transformadores'
+import Linhas from '../pages/Linhas'
 
 function AppRoutes() {
   return (
@@ -18,6 +22,10 @@ function AppRoutes() {
           <Route path="/monitoramento" element={<Monitoramento />} />
           <Route path="/alertas" element={<Alertas />} />
           <Route path="/eventos" element={<Eventos />} />
+          <Route path="/usinas" element={<Usinas />} />
+          <Route path="/subestacoes" element={<Subestacoes />} />
+          <Route path="/transformadores" element={<Transformadores />} />
+          <Route path="/linhas" element={<Linhas />} />
         </Route>
       </Routes>
     </BrowserRouter>
